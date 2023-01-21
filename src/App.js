840af49c8820566f7a0a3447497b9c5516424421
AlmiraKoshkina <almira.koshkina@gmail.com>
2023-01-21 11:40:57 -0500

    React Components, JSX and Lists

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import * as React from 'react';
 
-function getTitle(title){
-  return title;
-}
 
-
+const todoList = [
+{
+  id: 1,
+  title: 'Watch learning video',  
+  },
+  {
+    id: 2,
+  title: 'Complete assignment',  
+  },
+  {
+    id: 3,
+  title: 'Submit assignment',  
+  },
+];
 
 function App() {
+  
   return (
     <div>
-      <h1> Hello {getTitle('React')}</h1>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((item)=>(
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
